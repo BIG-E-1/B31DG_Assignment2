ae078ccc78dcd26423b8937186f61460260966b7
Ethan Hunking <ethan.hunking@gmail.com>
2022-02-24 15:51:51 +0000

    Trial of Ticker Example Code

--- B31DG_Assignment2/B31DG_Assignment2.ino
+++ B31DG_Assignment2/B31DG_Assignment2.ino
@@ -1,6 +1,22 @@
+#include <Ticker.h>
+
+Ticker periodicTicker;
+Ticker onceTicker;
+
+void periodicPrint(){
+  Serial.println("printing in periodic function");
+}
+
+void oncePrint(){
+  Serial.println("printing in once function");
+}
+
 void setup() {
   // put your setup code here, to run once:
-//test
+  Serial.begin(115200);
+
+  periodicTicker.attach_ms(5000, periodicPrint);
+  onceTicker.once_ms(10000, oncePrint);
 }
 
 void loop() {
