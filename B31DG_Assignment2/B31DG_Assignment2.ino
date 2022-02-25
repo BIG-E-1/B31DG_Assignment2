ad914a472cd3e50a5293e7b94af0beab74657de1
Ethan Hunking <ethan.hunking@gmail.com>
2022-02-25 00:42:36 +0000

    Ticker test

--- B31DG_Assignment2/B31DG_Assignment2.ino
+++ B31DG_Assignment2/B31DG_Assignment2.ino
@@ -2,9 +2,11 @@
 
 Ticker periodicTicker;
 Ticker onceTicker;
+int tick = 0;
 
 void periodicPrint(){
   Serial.println("printing in periodic function");
+  tick++;
 }
 
 void oncePrint(){
@@ -14,12 +16,13 @@ void oncePrint(){
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-
-  periodicTicker.attach_ms(5000, periodicPrint);
+  periodicTicker.attach_ms(0.1, periodicPrint);
   onceTicker.once_ms(10000, oncePrint);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  Serial.println(tick);
+  
 }
