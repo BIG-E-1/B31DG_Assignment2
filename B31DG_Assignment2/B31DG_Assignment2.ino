b91355b96928304e29b399004fe708e823fbbaa0
Ethan Hunking <ethan.hunking@gmail.com>
2022-03-03 15:35:27 +0000

    Task 2 added

--- B31DG_Assignment2/B31DG_Assignment2.ino
+++ B31DG_Assignment2/B31DG_Assignment2.ino
@@ -4,6 +4,11 @@ Ticker periodicTicker;
 Ticker onceTicker;
 int tick = 0;
 
+#define task2_pin 22
+int task2_state = 0;
+int task4_pin = A0;    // select the input pin for the potentiometer
+int task4_state = 0; 
+
 void periodicPrint(){
   Serial.println("printing in periodic function");
   tick++;
@@ -13,16 +18,42 @@ void oncePrint(){
   Serial.println("printing in once function");
 }
 
+//Task2 High/Low In 5Hz
+void task2(){                        
+  task2_state = digitalRead(task2_pin);
+}
+
+//Task3 Freq In 1Hz
+void task3(){                        
+}
+
+//Task4 Peteniotmeter 24Hz
+void task4(){     
+  task4_state = analogRead(task4_pin);                   
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  periodicTicker.attach_ms(0.1, periodicPrint);
-  onceTicker.once_ms(10000, oncePrint);
+  //periodicTicker.attach_ms(0.1, periodicPrint);
+  
+  pinMode(task2_pin, INPUT);    //Button Digital
+   
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  Serial.println(tick);
+ // Serial.println(tick);
+
+ while(1){
+ task2();
+ Serial.println(task2_state);
+ delay(1000);
+ }
+
+
+  
   
 }
